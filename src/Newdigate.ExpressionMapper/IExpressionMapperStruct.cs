7ac6dd9582765f4c4e11c6dc5161738feb39c5e4
Newdigate <nicnewdigate@gmail.com>
2022-03-17 21:30:23 +0000

    initial commit + 1

---
+++ src/Newdigate.ExpressionMapper/IExpressionMapperStruct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Newdigate.ExpressionMapper
+{
+    public interface IExpressionMapperStruct<TEntity, TDataRow, TStructReturnType> where TStructReturnType : struct
+    {
+        Expression<Func<TDataRow, TStructReturnType>> GetMappedExpression(Expression<Func<TEntity, bool>> source);
+    }
+
+}
+
